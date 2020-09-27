b11ac456bfa0a36998fd458426ebfef8a3c876f2
Lee Jia Keat <s10177804@connect.np.edu.sg>
2020-09-27 16:53:49 +0800

    Added command to toggle colosseum reminders

--- bot.py
+++ bot.py
@@ -34,7 +34,21 @@ async def called_once_a_day():
 @called_once_a_day.before_loop
 async def before():
     await bot.wait_until_ready()
-    print("Finished waiting")
+    print("Ready.")
     
+@bot.command(name="toggle")
+async def toggle_reminders(ctx):
+    message_channel = bot.get_channel(target_channel_id)
+    print(f"Got channel {message_channel}")
+    if called_once_a_day.is_running():
+        called_once_a_day.cancel()
+        print("Reminders disabled.")
+        await message_channel.send("Colosseum reminders are now off.")
+    else:
+        active = True
+        called_once_a_day.start()
+        print("Reminders enabled.")
+        await message_channel.send("Colosseum reminders are now on.")
+
 called_once_a_day.start()
 bot.run(TOKEN)
